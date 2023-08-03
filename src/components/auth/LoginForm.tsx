015089db09647e2688a2242f2cfb2fee8fa94462
조성록 <jsr4613@naver.com>
2023-08-03 22:47:43 +0900

    pjt setting

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Form, Input, Button } from "antd";
 import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import shortid from "shortid";
 
 const LoginForm: React.FC = () => {
+  const naviagate = useNavigate();
   const handleLogin = async (values: any) => {
-    alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
     // TODO: email과 password를 DB에서 찾아서 로그인 검증
     // TODO: 일치하는 유저가 없는 경우 "일치하는 유저를 찾을 수 없습니다." alert
     // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
@@ -13,6 +15,41 @@ const LoginForm: React.FC = () => {
     // TODO: 성공 시(2), localStorage에 token과 email을 저장
     // TODO: 성공 시(3), token은 shortId로 생성
     // TODO: 성공 시(4), "/" 라우터로 이동
+
+    const { email, password, confirmPassword, agreed } = values;
+
+    try {
+      const response = await axios.get(
+        // 이메일, 비밀번호 따로 검증 요청을 하려면, email, password를 따로 get요청
+        // ?를 기준으로 URL로 파라미터로 보냄
+        // get방식에서 &사인으로 구분
+        // `http://localhost:4000/users?email=${email}&password=${password}`
+        `http://localhost:4000/users?email=${values.email}&password=${values.password}`
+      );
+
+      // TODO: email과 password를 DB에서 찾아서 로그인 검증
+      if (response.data.length <= 0) {
+        // TODO: 일치하는 유저가 없는 경우 "일치하는 유저를 찾을 수 없습니다." alert
+        alert("일치하는 유저를 찾을 수 없습니다.");
+        return false;
+      } else {
+        // TODO: 성공 시(1), "로그인에 성공하였습니다. 메인 페이지로 이동합니다." alert
+        alert("로그인에 성공하였습니다. 메인 페이지로 이동합니다.");
+
+        // TODO: 성공 시(2), localStorage에 token과 email을 저장
+        // TODO: 성공 시(3), token은 shortId로 생성
+        // const loginUser = { token: shortid.generate(), email: email };
+        // localStorage.setItem("loginUser", JSON.stringify(loginUser));
+        localStorage.setItem("email", values.email);
+        localStorage.setItem("token", shortid.generate());
+
+        // TODO: 성공 시(4), "/" 라우터로 이동
+        naviagate(`/`);
+      }
+    } catch (error) {
+      // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
+      alert("일시적인 오류가 발생하였습니다. 고객센터로 연락주세요.");
+    }
   };
 
   return (
--- src/shared/AuthLayout.tsx
+++ src/shared/AuthLayout.tsx
@@ -8,13 +8,19 @@ const AuthLayout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
     // TODO: localStorage의 토큰 검색
+    const token = window.localStorage.getItem("token");
     // TODO: localStorage의 이메일 검색
+    const email = window.localStorage.getItem("email");
     // TODO: 토큰 또는 이메일 중 하나라도 없을 경우 "토큰 또는 이메일이 없습니다. 로그인해주세요." alert
-    // TODO: localStorage에 있는 token, email을 제거
-    // TODO: "/auth"로 이동
+    if (!token || !email) {
+      alert("토큰 또는 이메일이 없습니다. 로그인해주세요.");
+      // TODO: localStorage에 있는 token, email을 제거
+      window.localStorage.removeItem("token");
+      window.localStorage.removeItem("email");
+      // TODO: "/auth"로 이동
+      navigate("/auth");
+    }
   }, [navigate]);
 
   const handleLogoutButtonClick = () => {
@@ -24,6 +30,12 @@ const AuthLayout = () => {
     // TODO: yes 선택 시, localStorage의 token과 email 제거
     // TODO: "로그아웃이 완료되었습니다" alert
     // TODO: "/auth"로 이동
+    if (window.confirm("로그아웃 하시겠습니까?")) {
+      window.localStorage.removeItem("token");
+      window.localStorage.removeItem("email");
+      alert("로그아웃이 완료되었습니다.");
+      navigate("/auth");
+    }
   };
 
   return (
